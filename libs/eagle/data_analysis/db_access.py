3f17cde99ed84a9742dfff22f9027aca219bd408
Joe Lee <xhd0216@gmail.com>
2018-09-18 15:03:09 -0700

    refactor code

--- libs/eagle/data_analysis/db_access.py
+++ libs/eagle/data_analysis/db_access.py
@@ -1,18 +1,28 @@
+import json
 import requests
 import sqlalchemy as sa
 
 import cot_retriever.db as db
 
-headers = {'Accept-Encoding': 'gzip, deflate, br', 'Connection': 'keep-alive', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'Upgrade-Insecure-Requests': 1, 'Host': 'www.alphavantage.co', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+headers = {
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Connection': 'keep-alive',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  'Upgrade-Insecure-Requests': 1,
+  'Host': 'www.alphavantage.co',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+}
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=gld&apikey=GLP4NLROVIIPJAEP'
+token = 'GLP4NLROVIIPJAEP'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=%s&apikey=' + token
 
-resp = requests.get(url, params=headers)
+def get_historical_data(stock):
+  """ get weekly historical data for stock """
+  resp = requests.get(url % stock, params=headers)
 
-import json
+  js = json.loads(resp.content)
+  return js['Weekly Time Series']
 
-js = json.loads(resp.content)
-print json.dumps(js, indent=3)
 
 def test_data():
   engine = sa.create_engine('sqlite:///' + '../cot_retriever/data/DFO/db.db')
